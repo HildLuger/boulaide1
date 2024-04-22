a5a4f524dae0721bf375cc0e4fe2cd639bce7a59
HildLuger <88519194+HildLuger@users.noreply.github.com>
2024-04-22 00:34:47 -0300

    h optmization

--- src/app/h.tsx
+++ src/app/h.tsx
@@ -3,7 +3,7 @@ import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three-stdlib';
 
 const H: React.FC = () => {
-    const glb = useLoader(GLTFLoader, 'h3.glb');
+    const glb = useLoader(GLTFLoader, 'h4.glb');
     const scale = [0.2, 0.2, 0.2];  // Scale factors for x, y, and z
     const position = [0, 5, 0];  // Position coordinates x, y, z
 
